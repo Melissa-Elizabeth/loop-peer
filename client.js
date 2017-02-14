eddeafd79c5d773fba8e3dcf9502f359a8356de0
Avatar <avatar@Avatars-MacBook-Pro.local>
2017-02-14 11:21:34 -0600

    Updated forEach jQuery

--- client.js
+++ client.js
@@ -1,32 +1,49 @@
 $(document).ready(function() {
-var total = 0;
-var employeeArray = [
-  {name: 'Bob', yearsOfExperience: 10},
-  {name: 'Carla', yearsOfExperience: 4},
-  {name: 'Kris', yearsOfExperience: 14}
-];
+
+  var total = 0;
+  var employeeArray = [
+    {name: 'Bob', yearsOfExperience: 10},
+    {name: 'Carla', yearsOfExperience: 4},
+    {name: 'Kris', yearsOfExperience: 14}
+  ];
+
+  employeeArray.forEach(function(employee) {
+    total += parseFloat(employee.yearsOfExperience);
+
+    console.log(total);
+
+    $('table').append('<tr><td>' + employee.name + '</td><td>' + employee.yearsOfExperience +'</td></tr>');
+
+  });
+
+  $('span').text(total);
 
 
-employeeArray.forEach(function(employee) {
- total += parseFloat(employee.yearsOfExperience);
 
-console.log(total);
+  $('form').on('submit', function() {
+    event.preventDefault();
 
-$('table').append('<tr><td>' + employee.name + '</td><td>' + employee.yearsOfExperience +'</td></tr>');
+    var inputArray = $(this).serializeArray();
+    var newEmployeeObject = {};
+    console.log(inputArray);
 
-});
 
-$('button').on('click', function() {
-  var firstName = $('#firstName').val();
-  var years= $('#years').val();
-  employeeArray.push({name: firstName, yearsOfExperience: years});
-  console.log(employeeArray);
-$('table').append('<tr><td>' + firstName + '</td><td>' + years +'</td></tr>');
-  total += parseFloat(years);
+    inputArray.forEach(function(inputField){
+      newEmployeeObject[inputField.name] = inputField.value;
+    });
+    console.log(newEmployeeObject);
 
-  var grabSpan = $('span').text();
 
-  $('span').text(total += parseFloat(grabSpan));
-}); // end on click listener
+    $('table').append('<tr><td>' + newEmployeeObject.firstName + '</td><td>' + newEmployeeObject.yearsOfExperience +'</td></tr>');
+
+    total += parseFloat(newEmployeeObject.yearsOfExperience);
+
+
+    $('input').val('');
+
+    $('span').text(total);
+
+    // $('span').text(total += parseFloat(grabSpan));
+  }); // end on click listener
 
 }); // end doc ready
